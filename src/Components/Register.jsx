1801ab541813e7454824fc4e21112c904e76ee7b
unknown <shamiullipu@gmail.com>
2023-04-27 14:51:39 +0600

    login page added

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     const [email, setEmail] = useState('');
@@ -6,30 +7,39 @@ const Register = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-        console.log(email, password)
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, email, password)
     }
 
     return (
         <div className='container mx-auto'>
-            <div className="card w-full mt-6 bg-base-100 shadow-xl">
-                <div className="card-body items-center text-center">
+            <div className="card flex-shrink-0 w-full mt-4 shadow-2xl bg-base-100">
+                <div className="card-body items-center ">
                     <h2 className="card-title">Register Please!</h2>
 
                     <form action="" onSubmit={handleSubmit} >
+                        <label className="input-group input-group-md mb-2">
+                            <span className='w-1/3'>Name</span>
+                            <input type="text" name='name' placeholder="name" className="input input-bordered input-md w-full" required />
+                        </label>
                         <label className="input-group input-group-md">
                             <span className='w-1/3'>Email</span>
-                            <input onChange={(e) => setEmail(e.target.value)} type="text" name='email' placeholder="Enter email" className="input input-bordered input-md w-full" />
+                            <input onChange={(e) => setEmail(e.target.value)} type="text" name='email' placeholder="Enter email" className="input input-bordered input-md w-full" required />
                         </label>
                         <label className="input-group input-group-md py-2">
                             <span className='w-1/3'>Password</span>
-                            <input onChange={(e) => setPassword(e.target.value)} type="password" name='password' placeholder="password" className="input input-bordered input-md w-full" />
+                            <input onChange={(e) => setPassword(e.target.value)} type="password" name='password' placeholder="password" className="input input-bordered input-md w-full" required />
                         </label>
                         {/* <input type='submit' value='Register' className="btn btn-info" /> */}
-                        <button type='submit' className='btn btn-info'>Register</button>
+                        <button type='submit' className='btn btn-info w-full'>Register</button>
                     </form>
 
+                    <Link to='/login'>
+                        <p className="link link-primary">Already have an account?</p>
+                    </Link>
                 </div>
             </div>
         </div>
